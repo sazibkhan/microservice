3d56587b50e86da92f76e480e76e3d0a653ccd08
sazib khan <sazibkhancse@gmail.com>
2023-01-05 15:01:05 +0600

    Create Authentication Service

---
+++ auth-service/src/test/java/com/nagalay/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagalay.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
